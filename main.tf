fc99d06ca00146a532a0efb438512273649cb87e
Stefan Wessels Beljaars <swesselsbeljaars@schubergphilis.com>
2020-05-04 23:28:30 +0200

    rm role mgmt for now

--- main.tf
+++ main.tf
@@ -1,37 +1,5 @@
 data "aws_region" "current" {}
 
-data "aws_iam_policy_document" "default" {
-  statement {
-    actions = [
-      "sts:AssumeRole"
-    ]
-    principals {
-      type        = "Service"
-      identifiers = ["glue.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "default" {
-  count              = var.role_arn == null ? 1 : 0
-  name               = "GlueRole-${var.name}"
-  assume_role_policy = data.aws_iam_policy_document.default.json
-  tags               = var.tags
-}
-
-resource "aws_iam_role_policy" "default" {
-  count  = var.policy != null ? (var.role_arn != null ? 1 : 0) : 0
-  name   = "GlueRolePolicy-${var.name}"
-  role   = aws_iam_role.default[0].id
-  policy = var.policy
-}
-
-resource "aws_iam_role_policy_attachment" "default" {
-  count      = var.role_arn == null ? 1 : 0
-  role       = aws_iam_role.default[0].id
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
-}
-
 resource "aws_glue_job" "default" {
   name              = var.name
   connections       = var.connections
@@ -39,7 +7,7 @@ resource "aws_glue_job" "default" {
   glue_version      = var.glue_version
   max_capacity      = var.max_capacity
   max_retries       = var.max_retries
-  role_arn          = var.role_arn != null ? var.role_arn : aws_iam_role.default[0].arn
+  role_arn          = var.role_arn
   tags              = var.tags
 
   command {
--- outputs.tf
+++ outputs.tf
@@ -7,8 +7,3 @@ output "id" {
   value       = aws_glue_job.default.id
   description = "The Glue job name"
 }
-
-output "role_arn" {
-  value       = aws_iam_role.default[count.index].arn
-  description = "ARN of the IAM Role"
-}
