be0dd7086e9c96dbc29f89e674bc61f5af46e880
Jacob Verhoeks <jjverhoeks@schubergphilis.com>
2022-05-30 17:52:55 +0200

    Update parameters

--- main.tf
+++ main.tf
@@ -5,10 +5,10 @@ resource "aws_glue_job" "default" {
   connections       = var.connections
   default_arguments = var.default_arguments
   glue_version      = var.glue_version
-  max_capacity      = var.max_capacity
+  #max_capacity      = var.max_capacity
   max_retries       = var.max_retries
   role_arn          = var.role_arn
-  number_of_workers = var.number_of_workers
+  #number_of_workers = var.number_of_workers
   worker_type       = var.worker_type
   tags              = var.tags
 
